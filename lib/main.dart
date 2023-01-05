998108cc869d8395e91cc8e16544e6b1f3205807
Tanguy Hauguel <tanguy.hauguel@etudiant.univ-lr.fr>
2023-01-05 14:23:40 +0100

    first dice structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,89 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    // Disables debug tag which are in the flutter apps //
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Color.fromARGB(255, 53, 51, 75),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 53, 51, 75),
+        title: Text("421"),
+        centerTitle: true,
+      ),
+      body: Dice(),
+    ),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Dice extends StatefulWidget {
+  const Dice({Key? key}) : super(key: key);
 
   @override
+  _DiceState createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '421',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('421 menu'),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                MaterialButton(
+                  child:
+                      Image.asset('images/dice1.png', height: 150, width: 150),
+                  onPressed: () {},
+                ),
+                Text(
+                  "Player 1",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 10,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                MaterialButton(
+                  child: Image.asset(
+                    'images/dice1.png',
+                    height: 150,
+                    width: 150,
+                  ),
+                  onPressed: () {},
+                ),
+                Text(
+                  "Player 2",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 10,
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
-        body: const Center(
-          child: Text('Bienvenue sur le 421'),
+        // To create space between dice and result //
+        SizedBox(
+          height: 20,
         ),
-      ),
+        Text(
+          "result",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+      ],
     );
   }
 }
